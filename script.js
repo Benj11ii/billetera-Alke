168a8bdd20d0e6e8ec774359b4d624c621b6166b
Benj11ii <benjaminalonsocarmona@gmail.com>
2026-01-01 23:37:47 -0300

    Agrega funciones básicas al script, test

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
         error.textContent = "Usuario o contraseña incorrectos";
     }
 });
+
+function login() {
+    // 1. Obtener valores del formulario
+    let usuario = document.getElementById("usuario").value;
+    let clave = document.getElementById("clave").value;
+
+    // 2. Validar (Simulado)
+    if(usuario === "admin" && clave === "1234") {
+        alert("Bienvenido");
+        window.location.href = "menu.html"; // Redirigir al menú
+    } else {
+        alert("Datos incorrectos");
+    }
+}
+
+// Definir saldo inicial si no existe
+let saldo = localStorage.getItem("saldo") || 1000;
+
+// Función para mostrar saldo en pantalla
+document.getElementById("saldo-display").innerText = "$" + saldo;
+
+// Función para depositar
+function depositar() {
+    let monto = parseInt(document.getElementById("monto").value);
+    saldo = parseInt(saldo) + monto;
+    
+    // Guardar nuevo saldo en memoria del navegador
+    localStorage.setItem("saldo", saldo);
+    alert("Depósito exitoso");
+}
